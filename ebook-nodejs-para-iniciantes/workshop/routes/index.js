1719ff778d794d2859db52f7d7e58af685813334
Marcelo Borges <marcelo@timastertec.com.br>
2018-07-08 15:54:54 -0300

    Alterando os routers

--- ebook-nodejs-para-iniciantes/workshop/routes/index.js
+++ ebook-nodejs-para-iniciantes/workshop/routes/index.js
@@ -9,26 +9,5 @@ router.get('/', function(req, res, next) {
 	})
 });
 
-/*
-	OUTRO modelo para abrir uma rota
-	POST new 
-*/
-/*router.post('/new', function(req, res, next){
-	const nome = req.body.nome;
-	const idade =  parseInt(req.body.idade);
-	const uf = req.body.uf;
-	global.db.insert({nome, idade, uf}, (err, result) =>{
-		if(err){ return console.log(err); }
-		res.redirect('/');
-	})
-});*/
-
-router.get('/delete/:id', function(req, res){
-	var id = req.params.id;
-	global.db.deleteOne(id, (e, r) => {
-		if(e) {return console.log(e); }
-		res.redirect('/');
-	});
-});
 
 module.exports = router;
--- ebook-nodejs-para-iniciantes/workshop/routes/new.js
+++ ebook-nodejs-para-iniciantes/workshop/routes/new.js
@@ -6,22 +6,26 @@ router.get('/', function(req, res, next){
 	res.render('new', {title: 'Crud de Clientes'});
 });
 
-router.post('/', function(req, res, next){
+/*
+	OUTRO modelo para abrir uma rota
+	POST new 
+*/
+router.post('/new', function(req, res, next){
 	const nome = req.body.nome;
 	const idade =  parseInt(req.body.idade);
 	const uf = req.body.uf;
 	global.db.insert({nome, idade, uf}, (err, result) =>{
 		if(err){ return console.log(err); }
 		res.redirect('/');
-	});
-})
+	})
+});
 
-/*router.get('/delete/:id', function(req, res){
+router.get('/delete/:id', function(req, res){
 	var id = req.params.id;
 	global.db.deleteOne(id, (e, r) => {
 		if(e) {return console.log(e); }
 		res.redirect('/');
 	});
-});*/
+});
 
 module.exports = router;
